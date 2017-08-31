f1f2d778631d479d0facff648c5e4a678a2947cc
Andy Dev <andyalcantara745@yahoo.com>
2017-08-31 13:04:08 -0400

    Setting Headers

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
 
+//Setting headers
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 // Set our api routes
 app.use('/', api);
 app.use('/opinion', personalRoutes);
