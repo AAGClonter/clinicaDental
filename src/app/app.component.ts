fa5f153d7b166bf371d86a86ca576cfd3afd515c
Andy Dev <andyalcantara745@yahoo.com>
2017-11-18 17:13:04 -0500

    Adding the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CarouselService } from './carousel/carousel.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ CarouselService ]
 })
 export class AppComponent {
   title = 'app';
