3e69ee4f8b80d731891e81752da724ebcbdf47c1
Andy Dev <andyalcantara745@yahoo.com>
2018-02-25 12:28:20 -0500

    Adding images in routes

--- server/image.js
+++ server/image.js
@@ -21,19 +21,48 @@ router.get('/images', (req, res, next) => {
 // <POST>
 
 router.post('/image', (req, res, next) => {
+    const images = [
+        {
+            "slide": "0",
+            "image": "first-image",
+            "href": "../assets/images/carousel-images/concordancia.png"
+        },
+        {
+            "slide": "1",
+            "image": "second-image",
+            "href": "../assets/images/carousel-images/labios-BN-small.png"
+        },
+        {
+            "slide": "2",
+            "image": "third-image",
+            "href": "../assets/images/carousel-images/men.png"
+        },
+        {
+            "slide": "3",
+            "image": "forth-image",
+            "href": "../assets/images/carousel-images/perfect.png"
+        },
+        {
+            "slide": "4",
+            "image": "fifth-image",
+            "href": "../assets/images/carousel-images/sombrerito.png"
+        }
+    ];
+    for (image in images) {
     let image = new Image({
-        slide: req.body.slide,
-        image: req.body.image,
-        href: req.body.href
+        slide: image.slide,
+        image: image.image,
+        href: image.href
     });
 
-    image.save((err, image) => {
-        if (err) return next(err);
-        res.status(200).json({
-            message: 'Image created',
-            obj: image
+        image.save((err, image) => {
+            if (err) return next(err);
+            res.status(200).json({
+                message: 'Image created',
+                obj: image
+            });
         });
-    });
+    }
 });
 // </POST>
 
