7882efaf22d474a2d3aa43554d4baebd0406fa23
Andy Alcantara <andyalcantara745@yahoo.com>
2018-08-08 09:35:16 -0400

    Added google auth strategy from passport

--- server/user.js
+++ server/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router;
 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+
 const User = require('../models/user');
 
 router.post('/', (req, res, next) => {
